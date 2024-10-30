a03e5f47a614030a90c3166a0eea2b15b901a814
Cristian Villamil <cristian.jiovanyvillamilprieto@washpost.com>
2024-10-30 16:35:00 -0500

    Reset all the npm files and add again the file changes

--- libs/EntitiesDataClient.ts
+++ libs/EntitiesDataClient.ts
@@ -1,40 +1,40 @@
 import { client } from "./AmplifyDataClient";
 
-export const getAllEventUsers = async () => {
-  let _nextToken: any;
-  let _data;
-  while (_nextToken !== "") {
-    const { data, nextToken } = await client.models.EventsUser.list({
-      nextToken: _nextToken,
-    });
-    _data ? (_data = [_data, ...data]) : (_data = data);
-    nextToken ? (_nextToken = nextToken) : (_nextToken = "");
-  }
-  return _data;
+type ListResponse<T> = { data: T[]; nextToken?: string | null };
+type FilterInput = { eventId?: string; userId?: string };
+
+// Función auxiliar para obtener todos los registros de un modelo con paginación y opcionalmente un filtro
+const fetchAllData = async <T>(model: any, filter: any = null): Promise<T[]> => {
+  let nextToken: string | null = null;
+  const allData: T[] = [];
+
+  do {
+    const response: ListResponse<T> = await model.list({ nextToken, filter });
+    allData.push(...response.data);
+    nextToken = response.nextToken ?? null;
+  } while (nextToken);
+
+  return allData;
 };
 
+// Función específica para obtener todos los usuarios de eventos con filtro opcional
+export const getAllEventUsers = async (input: FilterInput = {}) => {
+  const filter = {
+    or: [
+      input.eventId ? { eventId: { eq: input.eventId } } : undefined,
+      input.userId ? { userId: { eq: input.userId } } : undefined,
+    ].filter(Boolean), // Elimina los filtros no definidos
+  };
+
+  return fetchAllData(client.models.EventsUser, filter);
+};
+
+// Función específica para obtener todos los usuarios
 export const getAllUsers = async () => {
-  let _nextToken: any;
-  let _data;
-  while (_nextToken !== "") {
-    const { data, nextToken } = await client.models.Users.list({
-      nextToken: _nextToken,
-    });
-    _data ? (_data = [_data, ...data]) : (_data = data);
-    nextToken ? (_nextToken = nextToken) : (_nextToken = "");
-  }
-  return _data;
+  return fetchAllData(client.models.Users);
 };
 
+// Función específica para obtener todos los eventos
 export const getAllEvents = async () => {
-  let _nextToken: any;
-  let _data;
-  while (_nextToken !== "") {
-    const { data, nextToken } = await client.models.Events.list({
-      nextToken: _nextToken,
-    });
-    _data ? (_data = [_data, ...data]) : (_data = data);
-    nextToken ? (_nextToken = nextToken) : (_nextToken = "");
-  }
-  return _data;
+  return fetchAllData(client.models.Events);
 };
