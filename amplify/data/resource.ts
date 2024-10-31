6df1c3e2a68ac7e7adf1ec6a47a8bf878a3556ec
Styven Maldonado <maldonadostyven@gmail.com>
2024-10-31 14:56:53 -0500

    fix add field event relation in Payments

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,4 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import {type ClientSchema, a, defineData} from "@aws-amplify/backend";
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -7,116 +7,118 @@ specifies that any unauthenticated user can "create", "read", "update",
 and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Users: a
-    .model({
-      userId: a.id().required(),
-      given_name: a.string(),
-      family_name: a.string(),
-      address: a.string(),
-      email: a.string(),
-      birthdate: a.string(),
-      phone_number: a.string(),
-      numero_documento: a.string(),
-      tipo_documento: a.string(),
-      city: a.string(),
-      country: a.string(),
-      userTypeId: a.json(),
-      permissions: a.string(),
-      rol: a.json(),
-      eventsUsers: a.hasMany("EventsUser","userId"),
-      interviews: a.hasMany("Interviews","userId"),
-      payment: a.hasMany("Payments","userId")
-    })
-    .authorization((allow) => [allow.guest()]),
+    Users: a
+        .model({
+            userId: a.id().required(),
+            given_name: a.string(),
+            family_name: a.string(),
+            address: a.string(),
+            email: a.string(),
+            birthdate: a.string(),
+            phone_number: a.string(),
+            numero_documento: a.string(),
+            tipo_documento: a.string(),
+            city: a.string(),
+            country: a.string(),
+            userTypeId: a.json(),
+            permissions: a.string(),
+            rol: a.json(),
+            eventsUsers: a.hasMany("EventsUser", "userId"),
+            interviews: a.hasMany("Interviews", "userId"),
+            payment: a.hasMany("Payments", "userId")
+        })
+        .authorization((allow) => [allow.guest()]),
 
     UsersTypes: a
-    .model({
-      userTypeId: a.id().required(),
-      name: a.string(),
-      description: a.string(),
-      isDefault: a.boolean(),
-    })
-    .authorization((allow) => [allow.guest()]),
+        .model({
+            userTypeId: a.id().required(),
+            name: a.string(),
+            description: a.string(),
+            isDefault: a.boolean(),
+        })
+        .authorization((allow) => [allow.guest()]),
 
     Interviews: a
-    .model({
-      interviewsId: a.id().required(),
-      isVerified: a.boolean(),
-      description: a.string(),
-      userId: a.string(),
-      users: a.belongsTo("Users","userId")
-    })
-    .authorization((allow) => [allow.guest()]),
+        .model({
+            interviewsId: a.id().required(),
+            isVerified: a.boolean(),
+            description: a.string(),
+            userId: a.string(),
+            users: a.belongsTo("Users", "userId")
+        })
+        .authorization((allow) => [allow.guest()]),
 
-      Events: a
-    .model({
-      eventId: a.id().required(),
-      type: a.string(),
-      name: a.string(),
-      location: a.string(),
-      location_link: a.string(),
-      datetime_start: a.datetime(),
-      datetime_end: a.datetime(),
-      description: a.string(),
-      photos: a.string().array(),
-      cancelPolicy: a.string(),
-      cost: a.json(),
-      cost_interview: a.json(),
-      percent_advance_payment: a.integer(),
-      instruction: a.json(),
-      eventsId: a.hasMany("EventsUser","eventId")
-    })
-    .authorization((allow) => [allow.guest()]),
+    Events: a
+        .model({
+            eventId: a.id().required(),
+            type: a.string(),
+            name: a.string(),
+            location: a.string(),
+            location_link: a.string(),
+            datetime_start: a.datetime(),
+            datetime_end: a.datetime(),
+            description: a.string(),
+            photos: a.string().array(),
+            cancelPolicy: a.string(),
+            cost: a.json(),
+            cost_interview: a.json(),
+            percent_advance_payment: a.integer(),
+            instruction: a.json(),
+            payment: a.hasMany("Payments", "eventId"),
+            eventsId: a.hasMany("EventsUser", "eventId")
+        })
+        .authorization((allow) => [allow.guest()]),
 
-      EventsUser: a
-    .model({
-      eventUsersId: a.id().required(),
-      medicalPreincription: a.json(),
-      suggestions: a.string(),
-      aditional_info: a.string(),
-      transport_description: a.string(),
-      available_seats_number: a.integer(),
-      available_seats: a.boolean(),
-      needs_transport: a.boolean(),
-      userId: a.string(),
-      eventId: a.string(),
-      rol: a.json(),
-      users: a.belongsTo("Users","userId"),
-      events: a.belongsTo("Events","eventId"),
-      payments: a.hasMany("Payments","eventUsersId")
+    EventsUser: a
+        .model({
+            eventUsersId: a.id().required(),
+            medicalPreincription: a.json(),
+            suggestions: a.string(),
+            aditional_info: a.string(),
+            transport_description: a.string(),
+            available_seats_number: a.integer(),
+            available_seats: a.boolean(),
+            needs_transport: a.boolean(),
+            userId: a.string(),
+            eventId: a.string(),
+            rol: a.json(),
+            users: a.belongsTo("Users", "userId"),
+            events: a.belongsTo("Events", "eventId"),
+            payments: a.hasMany("Payments", "eventUsersId")
 
-    })
-    .authorization((allow) => [allow.guest()]),
+        })
+        .authorization((allow) => [allow.guest()]),
 
-  Payments: a
-    .model({
-      paymentId: a.id().required(),
-      status: a.string(),
-      name: a.string(),
-      response_id: a.string(),
-      method: a.string(),
-      datetime:a.datetime(),
-      attachment: a.string(),
-      info: a.json(),
-      value: a.float(),
-      userId: a.string(),
-      eventUsersId: a.string(),
-      users: a.belongsTo("Users","userId"),
-      eventUsers: a.belongsTo("EventsUser","eventUsersId")
-    })
-    .authorization((allow) => [allow.guest()]),
+    Payments: a
+        .model({
+            paymentId: a.id().required(),
+            status: a.string(),
+            name: a.string(),
+            response_id: a.string(),
+            method: a.string(),
+            datetime: a.datetime(),
+            attachment: a.string(),
+            info: a.json(),
+            value: a.float(),
+            userId: a.string(),
+            eventUsersId: a.string(),
+            eventsId: a.string(),
+            users: a.belongsTo("Users", "userId"),
+            events: a.belongsTo("Users", "eventsId"),
+            eventUsers: a.belongsTo("EventsUser", "eventUsersId")
+        })
+        .authorization((allow) => [allow.guest()]),
 
 });
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "iam",
-  },
+    schema,
+    authorizationModes: {
+        defaultAuthorizationMode: "iam",
+    },
 });
-
 
 
 /*== STEP 2 ===============================================================
---
+++ server/api/payments/index.ts
@@ -0,0 +1,35 @@
+import {
+  getAllEvents,
+  getAllEventUsers, getAllPayments,
+  getAllUsers,
+} from "~/libs/EntitiesDataClient";
+
+export default defineEventHandler(async (event) => {
+  const { offset, limit, eventId, userId } = getQuery(event);
+
+  const [payments , users, events] = await Promise.all([
+    getAllPayments({ eventId, userId }),
+    getAllUsers(),
+    getAllEvents()
+  ]);
+
+  const usersMap = new Map(users.map(user => [user.id, user]));
+  const eventsMap = new Map(events.map(event => [event.id, event]));
+
+  const response = payments?.map(payment => ({
+    ...payment,
+    events: eventsMap.get(payment.eventId),
+    users: usersMap.get(payment.userId)
+  }));
+
+
+  const start = offset ?? 0;
+  const end = (offset ?? 0) + (limit ?? 50);
+  const slicedData = response.slice(start, end);
+
+  return {
+    count: response?.length,
+    data: slicedData,
+  };
+});
+
