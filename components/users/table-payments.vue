03df7460d182e16743f68af3614c1d1e1d228f54
Styven Maldonado <maldonadostyven@gmail.com>
2024-11-01 16:08:38 -0500

    Refactor table-payments.vue to use Composition API
    Refactored table-payments.vue to embrace Vue's Composition API for better readability and state management. Added reactive UI state handling, improved status color, and masked value logic for payment transactions. Updated template slots to display event and payment details consistently.

--- components/users/table-payments.vue
+++ components/users/table-payments.vue
@@ -1,41 +1,93 @@
-<script lang="ts">
-import { client } from "~/libs/AmplifyDataClient";
+<script setup lang="ts">
+import {reactive} from "vue";
+import {useListPayments} from "~/hooks/payments";
+import {formatCurrency, formatDate} from "../../libs/format";
 
-export default {
-  data: () => ({
-    itemsPerPage: 5,
-    serverItems: [],
-    loading: false,
-    totalItems: 0,
-  }),
-  methods: {
-    async loadItems() {
-      const route = useRoute();
-      const { data } = await client.models.EventsUser.list({
-        filter: {
-          userId: {
-            eq: route.query.id?.toString() || "",
-          },
-        },
-      });
-      // @ts-ignore
-      this.serverItems = data
-    },
-  },
+const route = useRoute();
+
+const tablePaymentsUIState = reactive({
+  itemsPerPage: 5,
+  serverItems: [],
+  isLoading: false,
+  totalItems: 0,
+});
+
+const loadItems = async () => {
+  tablePaymentsUIState.isLoading = true;
+  try {
+    const {data} = await useListPayments({
+      userId: route.params.id,
+    })
+    tablePaymentsUIState.serverItems = data.value?.data || [];
+  } finally {
+    tablePaymentsUIState.isLoading = false;
+  }
 };
+
+const getColor = (value: string) => {
+  switch (value) {
+    case "PENDING":
+      return "bg-yellow-400";
+    case "APPROVED":
+      return "bg-green-400";
+    case "DECLINED":
+      return "bg-red-400";
+    case "VOIDED":
+      return "bg-red-400";
+    case "ERROR":
+      return "bg-red-400";
+    default:
+      return "bg-black"; // Valor por defecto
+  }
+}
+
+const getMaskValue = (value: string) => {
+  switch (value) {
+    case "PENDING":
+      return "Pendiente";
+    case "APPROVED":
+      return "Aprobado";
+    case "DECLINED":
+      return "Declinado";
+    case "VOIDED":
+      return "Declinado";
+    case "ERROR":
+      return "Error";
+    default:
+      return "Error"; // Valor por defecto
+  }
+}
 </script>
+
 <template>
+  <loading :isLoading="tablePaymentsUIState.isLoading"/>
   <v-data-table-server
-    v-model:items-per-page="itemsPerPage"
-    :headers="[
-      { title: 'Evento', key: 'status', align: 'start' },
-      { title: 'Pago', key: 'datetime', align: 'start' },
-      { title: 'Fecha', key: 'value', align: 'end' },
+      v-model:items-per-page="tablePaymentsUIState.itemsPerPage"
+      :headers="[
+      { title: 'Transaccion', key: 'payment', align: 'start' },
+      { title: 'Evento', key: 'events', align: 'start' },
     ]"
-    :items="serverItems"
-    :items-length="totalItems"
-    :loading="loading"
-    item-value="name"
-    @update:options="loadItems"
-  ></v-data-table-server>
+      :items="tablePaymentsUIState.serverItems"
+      :items-length="tablePaymentsUIState.totalItems"
+      :loading="tablePaymentsUIState.isLoading"
+      item-value="name"
+      @update:options="loadItems"
+  >
+    <template v-slot:item.events="{ value }">
+      <div v-if="value" class="grid py-3">
+        <span class="font-bold">{{ value.name }}</span>
+        <span>{{ value.location }}</span>
+        <span>{{ formatDate(new Date(value.datetime_start)) }}</span>
+      </div>
+    </template>
+    <template v-slot:item.payment="{ value }">
+      <div v-if="value" class="grid py-3">
+        <span :class="getColor(value.status) + ' ' +'px-3 py-1.5 rounded-lg w-fit'">{{
+            getMaskValue(value.status)
+          }}</span>
+        <span>${{ formatCurrency(value.value / 100, 'es-ES', 'COP') }}</span>
+        <span>{{ value.name }}</span>
+      </div>
+    </template>
+  </v-data-table-server>
 </template>
